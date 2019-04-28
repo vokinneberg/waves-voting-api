64fdfd40f2e8469d723dbbd17a34915a61bb94cb
vokinneberg <evgeniy81@gmail.com>
2019-04-28 19:45:17 +0300

    Improve snapshot job.

--- cron-jobs/snapshotJob.js
+++ cron-jobs/snapshotJob.js
@@ -23,99 +23,86 @@ export default class SnapshotJob {
 
             this._logger.info(`${projects.length} projects found.`);
 
-            async.forEach(
-                projects,
-                async project => {
-                    let projectRank = project.rank;
-                    const projectVotes = project.votes;
+            projects.forEach(async (project) => {
+                let origProjectRank = project.rank;
+                const projectVotes = project.votes;
 
-                    async.forEach(
-                        projectVotes,
-                        async vote => {
-                            const stake = this._wavesHelper.checkAssetStake(
-                                vote.waves_address,
-                                this._config.votingAssetId
-                            );
-                            const currentVoteRank = Math.log(stake);
-                            switch (vote.status) {
-                                case VoteStatus.Init:
-                                    if (vote.transaction_id) {
-                                        const transaction = await this._wavesHelper.checkTransaction(
-                                            vote.transaction_id
-                                        );
-                                        if (!transaction) {
-                                            throw new ObjectNotFoundError(
-                                                `Transaction ${
-                                                    vote.transaction_id
-                                                } not found in blockchain.'`
-                                            );
-                                        }
-                                        if (
-                                            stake <
-                                            this._config.votingMinumumStake
-                                        ) {
-                                            this._logger.info(
-                                                'Not enough funds to confirm vote.'
-                                            );
-                                            vote.status = VoteStatus.NoFunds;
-                                        } else {
-                                            projectRank += currentVoteRank;
-                                            vote.status = VoteStatus.Settled;
-                                        }
-                                    } else {
-                                        this._logger.info(
-                                            `No transaction for vote with address ${
-                                                vote.waves_address
-                                            }.`
+                projectVotes.forEach(async (vote, index) => {
+                        const stake = this._wavesHelper.checkAssetStake(
+                            vote.waves_address,
+                            this._config.votingAssetId
+                        );
+                        const currentVoteRank = Math.log(stake);
+                        switch (vote.status) {
+                            case VoteStatus.Init:
+                                if (vote.transaction_id) {
+                                    const transaction = await this._wavesHelper.checkTransaction(
+                                        vote.transaction_id
+                                    );
+                                    if (!transaction) {
+                                        throw new ObjectNotFoundError(
+                                            `Transaction ${
+                                                vote.transaction_id
+                                            } not found in blockchain.'`
                                         );
                                     }
-                                    break;
-                                case VoteStatus.NoFunds:
                                     if (
-                                        stake < this._config.votingMinumumStake
+                                        stake <
+                                        this._config.votingMinumumStake
                                     ) {
                                         this._logger.info(
                                             'Not enough funds to confirm vote.'
                                         );
+                                        project.votes[index].status = VoteStatus.NoFunds;
                                     } else {
-                                        projectRank += currentVoteRank;
-                                        vote.status = VoteStatus.Settled;
+                                        project.rank += currentVoteRank;
+                                        project.votes[index].status = VoteStatus.Settled;
                                     }
-                                    break;
-                                case VoteStatus.Settled:
-                                    if (
-                                        stake < this._config.votingMinumumStake
-                                    ) {
-                                        this._logger.info(
-                                            'Not enough funds. Revoke vote.'
-                                        );
-                                        projectRank -= currentVoteRank;
-                                        vote.status = VoteStatus.NoFunds;
-                                    }
-                                    break;
-                                default:
-                                    this._logger.error('Unknown vote status.');
-                            }
-                        },
-                        err => {
-                            if (err) throw err;
+                                } else {
+                                    this._logger.info(
+                                        `No transaction for vote with address ${
+                                            vote.waves_address
+                                        }.`
+                                    );
+                                }
+                                break;
+                            case VoteStatus.NoFunds:
+                                if (
+                                    stake < this._config.votingMinumumStake
+                                ) {
+                                    this._logger.info(
+                                        'Not enough funds to confirm vote.'
+                                    );
+                                } else {
+                                    project.rank += currentVoteRank;
+                                    project.votes[index].status = VoteStatus.Settled;
+                                }
+                                break;
+                            case VoteStatus.Settled:
+                                if (
+                                    stake < this._config.votingMinumumStake
+                                ) {
+                                    this._logger.info(
+                                        'Not enough funds. Revoke vote.'
+                                    );
+                                    project.rank -= currentVoteRank;
+                                    project.votes[index].status = VoteStatus.NoFunds;
+                                }
+                                break;
+                            default:
+                                this._logger.error('Unknown vote status.');
                         }
-                    );
-
-                    if (project.rank !== projectRank) {
-                        if (projectRank >= this._config.votingMaximumRank) {
-                            project.verification_status =
-                                ProjectVerificationStatus.Verified;
-                        }
-                        project.rank = projectRank;
-                        project.votes = projectVotes;
-                        await project.save();
                     }
-                },
-                err => {
-                    if (err) throw err;
+                );
+
+                if (project.rank !== origProjectRank) {
+                    if (project.rank >= this._config.votingMaximumRank) {
+                        project.verification_status =
+                            ProjectVerificationStatus.Verified;
+                    }
+                    await project.save();
                 }
-            );
+            });
 
             this._logger.info(`Cron-job SnapshotJob finished.`);
         } catch (error) {
